3be22a53d7a09d1d9ba0789249c9dff1cce4a5d0
barezzali2 <barezrealy22@gmail.com>
2025-08-18 15:30:55 +0300

    nav created for mobile

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+
+function Logo() {
+  return (
+    <div className="">
+        <img src="/assets/logo.png" alt="logo" className="w-22" />
+    </div>
+  )
+}
+
+export default Logo;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react'
+import Logo from './Logo'
+import { Link, NavLink } from 'react-router-dom'
+
+function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    function handleSetMenu() {
+        setMenuOpen((prev) => !prev);
+    }
+  return (
+    <div className='flex flex-row overflow-hidden fixed top-0 w-full flex-wrap items-center justify-around '>
+            <Logo />
+
+            <div className='w-40'></div>
+
+
+            <div
+            onClick={handleSetMenu}
+            className="relative w-[30px] h-[25px] flex flex-col justify-center items-center cursor-pointer md:hidden z-[1002]"
+            >
+                {/* Top Bar */}
+                <span
+                    className={`absolute w-full h-[4.0px] bg-white/85 rounded-[3px] transition-all duration-200 ease-in-out
+                    ${menuOpen ? 'rotate-45' : '-translate-y-[8px]'}`}
+                ></span>
+
+                {/* Middle Bar */}
+                <span
+                    className={`absolute w-full h-[4.0px] bg-white/85 rounded-[3px] transition-all duration-200 ease-in-out
+                    ${menuOpen ? 'opacity-0' : 'opacity-100'}`}
+                ></span>
+
+                {/* Bottom Bar */}
+                <span
+                    className={`absolute w-full h-[4.0px] bg-white/85 rounded-[3px] transition-all duration-200 ease-in-out
+                    ${menuOpen ? '-rotate-45' : 'translate-y-[8px]'}`}
+                ></span>
+            </div>
+
+
+        
+        <ul className={`flex flex-col w-full list-none overflow-hidden transition-all duration-400 ease-in-out text-center gap-8  ${menuOpen ? 'h-[310px] opacity-100 visible mt-[25px] ' : 'h-0 opacity-0 invisible'}`}>
+            <li>
+                <NavLink 
+                to="/"
+                className="text-xl font-semibold text-white transition-colors"
+                onClick={handleSetMenu}
+                >
+                    HOME
+                </NavLink>
+            </li>
+
+            <li>
+                <NavLink 
+                to="/services"
+                className="text-xl font-semibold text-white transition-colors"
+                onClick={handleSetMenu}
+                >
+                    SERVICES
+                </NavLink>
+            </li>
+
+            <li>
+                <NavLink 
+                to="/projects"
+                className="text-xl font-semibold text-white transition-colors"
+                onClick={handleSetMenu}
+                >
+                    PROJECTS
+                </NavLink>
+            </li>
+
+            <li>
+                <NavLink 
+                to="/about"
+                className="text-xl font-semibold text-white transition-colors"
+                onClick={handleSetMenu}
+                >
+                    ABOUT US
+                </NavLink>
+            </li>
+
+            <li>
+                <NavLink 
+                to="/contact"
+                className="text-xl font-semibold text-white transition-colors"
+                onClick={handleSetMenu}
+                >
+                    CONTACT
+                </NavLink>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
