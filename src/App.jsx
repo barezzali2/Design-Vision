d3ff5bb9c902a81564da14c22a2a68e44b33c831
barezzali2 <barezrealy22@gmail.com>
2025-08-21 18:16:22 +0300

    Adding scroll for the router

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import Services from "./pages/Services";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import ScrollToUp from "./components/ScrollToUp";
 
 function App() {
   return (
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="services" element={<Services />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-        </Routes>
+        <ScrollToUp />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="services" element={<Services />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+          </Routes>
       </BrowserRouter>
   )
 }
