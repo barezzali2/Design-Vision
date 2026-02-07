72253c67029f0de5601085c6131244ad0c1a81f4
barezzali2 <barezrealy22@gmail.com>
2026-02-07 09:12:11 +0300

    Adding project title

--- src/components/projects/ProjectsPage.jsx
+++ src/components/projects/ProjectsPage.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { motion } from "framer-motion"
 
 function ProjectsPage() {
   return (
-    <div className='mt-20 md:mt-15'>ProjectsPage</div>
+    <motion.div 
+    className='mt-20 md:mt-15 mx-auto px-4'
+    initial={{ opacity: 0, y:30 }}
+    whileInView={{ opacity: 1, y:0 }}
+    viewport={{once: true}}
+    transition={{duration: 1.4}}
+    >
+
+      <header className='text-center mb-12'>
+        <h1 className='text-[25px] md:text-4xl font-extrabold text-white mb-4'>
+          Our Cutting Edge Projects
+        </h1>
+        <p className='text-lg text-gray-500 max-w-3xl mx-auto'>
+          Explore our different projects with having an exclusive vision
+        </p>
+      </header>
+
+    </motion.div>
   )
 }
 
