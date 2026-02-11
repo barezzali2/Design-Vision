ebef30c7df9b6b3ffed7c5e3f771d4fc49fba180
barezzali2 <barezrealy22@gmail.com>
2026-02-11 08:30:35 +0300

    Adding language button in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,19 @@ import { FaFacebook } from "react-icons/fa";
 import { AiFillTikTok } from "react-icons/ai";
 import { SiThreads } from "react-icons/si";
 
+import { useTranslation } from 'react-i18next';
+
+
 function Footer() {
+  const { t } = useTranslation();
+
   return (
-    <footer className="text-center py-10 shadow-[0_0px_200px_0px_#0f1218] mt-20">
+    <footer className="text-center py-10 shadow-[0_0px_200px_0px_#0f1218]">
       <div className="max-w-6xl mx-auto">
         {/* Contact Section */}
         <div className="mb-10 md:flex md:justify-between md:text-center md:gap-100 md:mb-2">
           <div className=" mb-12 md:mb-0 md:w-1/2 md:pr-8 ">
-          <h2 className="text-2xl font-semibold mb-4 text-white">Get in touch</h2>
+          <h2 className="text-2xl font-semibold mb-4 text-white">{t('footer.contactTitle')}</h2>
           <div className="flex flex-col items-center space-y-5 mb-7">
             <a href="#" className="flex items-center space-x-2 text-gray-200 hover:text-gray-800 transition-colors md:hover:text-violet-500">
               <MdOutlineMail className="text-xl" />
@@ -26,7 +31,7 @@ function Footer() {
             </a>
             <a href="#" className="flex items-center space-x-2 text-gray-200 hover:text-gray-800 transition-colors md:hover:text-violet-500">
               <IoLocationOutline className="text-xl" />
-              <span>Green City, Sulaimani</span>
+              <span>{t('footer.location')}</span>
             </a>
           </div>
           
@@ -46,29 +51,28 @@ function Footer() {
 
         {/* About Section */}
         <div className="mb-12 px-7 md:w-1/2 md:pl-8 md:px-0">
-          <h2 className="text-2xl font-semibold mb-4 text-white">About Design Vision</h2>
+          <h2 className="text-2xl font-semibold mb-4 text-white">{t('footer.aboutDV')}</h2>
           <p className="text-gray-200 leading-relaxed max-w-2xl mx-auto">
-            We’d be happy to connect with you. Whether you’re looking to collaborate, need information, or just want to get in touch, 
-            <br />do not hesitate, you can reach us anytime using the contact details below.
+            {t('footer.aboutText')}
           </p>
         </div>
       </div>
 
         {/* Quick Links Section */}
         <div className="mb-6 md:mb-9">
-          <h2 className="text-2xl font-semibold mb-4 text-white md:mb-5">Quick Links</h2>
+          <h2 className="text-2xl font-semibold mb-4 text-white md:mb-5">{t('footer.quickLinks')}</h2>
           <div className="flex justify-center space-x-5 flex-wrap md:space-x-12">
             <Link to="/projects" className="text-gray-300 hover:text-indigo-400 transition-colors text-[14px] md:text-[17px]">
-              Portfolio
+              {t('footer.portfolio')}
             </Link>
             <Link to="/projects" className="text-gray-300 hover:text-indigo-400 transition-colors text-[14px] md:text-[17px]">
-              Logos
+              {t('footer.logos')}
             </Link>
             <Link to="/services" className="text-gray-300 hover:text-indigo-400 transition-colors text-[14px] md:text-[17px]">
-              Explore Services
+              {t('footer.services')}
             </Link>
             <Link to="/services" className="text-gray-300 hover:text-indigo-400 transition-colors text-[14px] md:text-[17px]">
-              Special Offers
+              {t('footer.offers')}
             </Link>
           </div>
         </div>
@@ -76,7 +80,7 @@ function Footer() {
         {/* Copyright */}
         <div className="border-t border-gray-200 pt-10">
           <p className="text-gray-300 text-sm md:text-[15px]">
-            © 2025 DesignVision. All rights reserved.
+            {t('footer.copyright')}
           </p>
         </div>
       </div>
